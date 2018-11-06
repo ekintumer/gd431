b88419412a0a6296ec15e9fd435d0038e4ba503c
ekintumer <etumer@mica.edu>
2018-11-05 23:06:02 -0500

    turn into responsive site

--- redesign/src/js/menu.js
+++ redesign/src/js/menu.js
@@ -1,8 +1,8 @@
-// var menuTrigger = document.getElementById("js-nav-trigger");
+// var menuTrigger = document.getElementById("js-menu-button");
 // menuTrigger.addEventListener("click", function () {
 //     let expanded = this.getAttribute("aria-expanded") === "true" || false;
 //     this.setAttribute("aria-expanded", !expanded);
-//     let menu = document.getElementById("js-site-nav");
+//     let menu = document.getElementById("js-menu");
 //     let visible = menu.getAttribute("data-visible") === "true" || false;
 //     menu.setAttribute("data-visible", !visible);
 //     // if (!visible === true) {
@@ -11,4 +11,26 @@
 //     // 	document.body.removeAttribute("style");
 //     // }
 // });
-// //
+
+//accordion
+
+(function ($) {
+    $('.accordion > li:eq(0) a').addClass('active').next().slideDown();
+
+    $('.accordion a').click(function (j) {
+        var dropDown = $(this).closest('li').find('p');
+
+        $(this).closest('.accordion').find('p').not(dropDown).slideUp();
+
+        if ($(this).hasClass('active')) {
+            $(this).removeClass('active');
+        } else {
+            $(this).closest('.accordion').find('a.active').removeClass('active');
+            $(this).addClass('active');
+        }
+
+        dropDown.stop(false, true).slideToggle();
+
+        j.preventDefault();
+    });
+})(jQuery);
