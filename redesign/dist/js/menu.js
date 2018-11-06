d4c0fd7fc015d65df6cb5b79622ad63926a9a250
ekintumer <etumer@mica.edu>
2018-11-06 13:05:04 -0500

    insert initial navigation/menu

--- redesign/dist/js/menu.js
+++ redesign/dist/js/menu.js
@@ -1,16 +1,18 @@
-// var menuTrigger = document.getElementById("js-menu-button");
-// menuTrigger.addEventListener("click", function () {
-//     let expanded = this.getAttribute("aria-expanded") === "true" || false;
-//     this.setAttribute("aria-expanded", !expanded);
-//     let menu = document.getElementById("js-menu");
-//     let visible = menu.getAttribute("data-visible") === "true" || false;
-//     menu.setAttribute("data-visible", !visible);
-//     // if (!visible === true) {
-//     // 	document.body.style.overflowY = "hidden";
-//     // } else {
-//     // 	document.body.removeAttribute("style");
-//     // }
-// });
+//nav
+
+var menuTrigger = document.getElementById("js-menu-button");
+menuTrigger.addEventListener("click", function () {
+    let expanded = this.getAttribute("aria-expanded") === "true" || false;
+    this.setAttribute("aria-expanded", !expanded);
+    let menu = document.getElementById("js-menu");
+    let visible = menu.getAttribute("data-visible") === "true" || false;
+    menu.setAttribute("data-visible", !visible);
+    // if (!visible === true) {
+    // 	document.body.style.overflowY = "hidden";
+    // } else {
+    // 	document.body.removeAttribute("style");
+    // }
+});
 
 //accordion
 
