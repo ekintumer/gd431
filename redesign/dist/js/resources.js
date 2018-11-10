f1e0dc0b338b1442c4fff056fba92e5592538684
ekintumer <etumer@mica.edu>
2018-11-09 22:47:42 -0500

    add read more

---
+++ redesign/dist/js/resources.js
@@ -0,0 +1,23 @@
+// var tabTrigger = document.addClass("js-tab-button");
+// tabTrigger.addEventListener("click", function () {
+//     let expanded = this.getAttribute("aria-expanded") === "true" || false;
+//     this.setAttribute("aria-expanded", !expanded);
+// });
+
+
+//read more 
+
+$(document).ready(function () {
+    $(".toggle").click(function () {
+        var elem = $(".toggle").text();
+        if (elem == "+ read more") {
+
+            $(".toggle").text("– read less");
+            $(".more").slideDown();
+        } else {
+
+            $(".toggle").text("+ read more");
+            $(".more").slideUp();
+        }
+    });
+});
