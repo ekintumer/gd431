a0cce438e77b2106ba8718ee1399b59f635650ef
ekintumer <etumer@mica.edu>
2018-11-11 22:39:00 -0500

    attempt tab js

--- redesign/dist/js/resources.js
+++ redesign/dist/js/resources.js
@@ -1,10 +1,27 @@
-// var tabTrigger = document.addClass("js-tab-button");
-// tabTrigger.addEventListener("click", function () {
-//     let expanded = this.getAttribute("aria-expanded") === "true" || false;
-//     this.setAttribute("aria-expanded", !expanded);
-// });
+//tab1
+
+var tab1Trigger = document.getElementById("js-tab-first");
+tab1Trigger.addEventListener("click", function () {
+    let expanded = this.getAttribute("aria-expanded") === "true" || false;
+    this.setAttribute("aria-expanded", !expanded);
+    let tab1 = document.getElementById("js-tab-1");
+    let visible = tab1.getAttribute("data-visible") === "true" || false;
+    tab1.setAttribute("data-visible", !visible);
+});
+
+//tab2
+
+var tab2Trigger = document.getElementById("js-tab-second");
+tab2Trigger.addEventListener("click", function () {
+    let expanded = this.getAttribute("aria-expanded") === "true" || false;
+    this.setAttribute("aria-expanded", !expanded);
+    let tab2 = document.getElementById("js-tab-2");
+    let visible = tab2.getAttribute("data-visible") === "true" || false;
+    tab2.setAttribute("data-visible", !visible);
+});
 
 //read more 
+
 $(document).ready(function () {
     $(".toggle").click(function () {
         var elem = $(".toggle").text();
